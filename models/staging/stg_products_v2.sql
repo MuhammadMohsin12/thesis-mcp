78cf54b39d550f8e5dd7a672827b09ca692d4c6d
MuhammadMohsin12 <46193350+MuhammadMohsin12@users.noreply.github.com>
2026-09-06 06:19:45 +0200

    Add staging model stg_products_v2

---
+++ models/staging/stg_products_v2.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+/*
+    Staging model: stg_products_v2
+    Source: {{ source('raw', 't_products') }}
+    Generated: 2026-09-06T04:19:31Z
+*/
+
+with source as (
+    select * from {{ source('raw', 't_products') }}
+),
+
+renamed as (
+    select
+        INDEX as index,
+        NAME as name,
+        BRAND as brand,
+        CATEGORY as category,
+        PRICE as price,
+        STOCK as stock
+    from source
+)
+
+select * from renamed
